72ec620c2dc130bfef6c09ae2533a9d1cfd7fe0b
MelsenC <melsen.carlsburg@gmail.com>
2022-11-18 13:00:51 -0500

    Move status validation and archived? method from
    Article and Comment classes to the Visible module

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -1,5 +1,13 @@
 module Visible
-    def archived?
-      status == 'archived'
-    end
+  extend ActiveSupport::Concern
+
+  VALID_STATUSES = ['public', 'private', 'archived']
+
+  included do
+    validates :status, inclusion: { in: VALID_STATUSES }
   end
+
+  def archived?
+    status == 'archived'
+  end
+end
